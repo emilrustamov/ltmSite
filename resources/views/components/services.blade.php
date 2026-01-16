f385decdc5ea81e6d7beafdef2bdd85e41c71dce
bibikagurbangulyeva <ogulbibigurbangulyeva@gmail.com>
2026-01-16 16:31:37 +0500

    Refactor service card layout and styles
    Updated service card heights and adjusted classes for better styling consistency.

--- resources/views/components/services.blade.php
+++ resources/views/components/services.blade.php
@@ -4,61 +4,73 @@
         <div class="swiper mySwiper">
            
             <div class="swiper-wrapper !h-[520px] py-16">
-                <div class="swiper-slide  max-w-[400px] h-[560px] p-6 rounded-xl shadow-lg bg-[#350000] flex flex-col fade-in"
+
+
+                
+<div class="swiper-slide  max-w-[400px] h-[660px] p-6 rounded-xl shadow-lg bg-[#350000] flex flex-col fade-in"
                     itemscope itemtype="http://schema.org/Service">
-                    <svg class="w-10 h-10 mb-4 text-white" fill="none" stroke="currentColor" stroke-width="2"
+                    <div class="flex flex-col h-full">
+                        <svg class="w-10 h-10 mb-4 text-white" fill="none" stroke="currentColor" stroke-width="2"
                         viewBox="0 0 24 24">
                         <path d="M3 4a1 1 0 011-1h16a1 1 0 011 1v16a1 1 0 01-1 1H4a1 1 0 01-1-1V4z" />
                         <path d="M3 8h18" />
                     </svg>
                     <h3 class="text-6xl font-bold mb-4" itemprop="name">{{ __('translate.servTitle1') }}</h3>
-                    <p class="text-3xl flex-grow mb-4" itemprop="description">
+                    <p class="text-3xl flex-grow mb-4 m-0" itemprop="description">
                         {{ __('translate.servDesc1') }}
                     </p>
                     <a href="/{{ $lang }}/portfolio"
-                        class="inline-block !px-10 !py-6 bg-[#bc2c2c] hover:bg-[#e31e24] rounded font-bold text-white text-2xl">
+                        class="mt-auto w-fit !px-10 !py-6 bg-[#bc2c2c] hover:bg-[#e31e24] rounded font-bold text-white text-2xl mt-auto">
                         {{ __('translate.readMore') }}
                     </a>
+                    </div>
                 </div>
 
-                <div class="swiper-slide  max-w-[400px] h-[560px] p-6 rounded-xl shadow-lg bg-[#bc2c2c] flex flex-col fade-in"
+                <div class="swiper-slide  max-w-[400px] h-[660px] p-6 rounded-xl shadow-lg bg-[#bc2c2c] flex flex-col fade-in"
                     itemscope itemtype="http://schema.org/Service">
-                    <svg class="w-10 h-10 mb-4 text-white" fill="none" stroke="currentColor" stroke-width="2"
+                    <div class="flex flex-col h-full">
+                        <svg class="w-10 h-10 mb-4 text-white" fill="none" stroke="currentColor" stroke-width="2"
                         viewBox="0 0 24 24">
                         <rect x="7" y="2" width="10" height="20" rx="2" />
                         <path d="M11 18h2" />
                     </svg>
                     <h3 class="text-6xl font-bold mb-4" itemprop="name">{{ __('translate.servTitle2') }}</h3>
-                    <p class="text-3xl flex-grow mb-4" itemprop="description">
+                    <p class="text-3xl flex-grow mb-4 m-0" itemprop="description">
                         {{ __('translate.servDesc2') }}
                     </p>
-                    <a href="/{{ $lang }}/services-mobileapps"
-                        class="inline-block !px-10 !py-6 bg-[#350000] hover:bg-[#e31e24] rounded font-bold text-white text-2xl">
+                    <a href="/{{ $lang }}/portfolio"
+                        class="mt-auto w-fit !px-10 !py-6 bg-[#350000] hover:bg-[#e31e24] rounded font-bold text-white text-2xl">
                         {{ __('translate.readMore') }}
                     </a>
+                    </div>
                 </div>
 
-                <div class="swiper-slide  max-w-[400px] h-[560px] p-6 rounded-xl shadow-lg bg-[#e31e24] flex flex-col fade-in"
+
+
+                <div class="swiper-slide  max-w-[400px] h-[660px] p-6 rounded-xl shadow-lg bg-[#e31e24] flex flex-col fade-in"
                     itemscope itemtype="http://schema.org/Service">
-                    <svg class="w-10 h-10 mb-4 text-white" fill="none" stroke="currentColor" stroke-width="2"
+                    <div class="flex flex-col h-full">
+                        <svg class="w-10 h-10 mb-4 text-white" fill="none" stroke="currentColor" stroke-width="2"
                         viewBox="0 0 24 24">
                         <circle cx="9" cy="21" r="1" />
                         <circle cx="20" cy="21" r="1" />
                         <path d="M1 1h4l2.68 13.39a2 2 0 002 1.61h9.72a2 2 0 002-1.61L23 6H6" />
                     </svg>
                     <h3 class="text-6xl font-bold mb-4" itemprop="name">{{ __('translate.servTitle3') }}</h3>
-                    <p class="text-3xl flex-grow mb-4" itemprop="description">
+                    <p class="text-3xl flex-grow mb-4 m-0" itemprop="description">
                         {{ __('translate.servDesc3') }}
                     </p>
-                    <a href="/{{ $lang }}/services-ecommerce"
-                        class="inline-block !px-10 !py-6 bg-[#350000] hover:bg-[#bc2c2c] rounded font-bold text-white text-2xl">
+                    <a href="/{{ $lang }}/portfolio"
+                        class="mt-auto w-fit !px-10 !py-6 bg-[#350000] hover:bg-[#bc2c2c] rounded font-bold text-white text-2xl mt-auto">
                         {{ __('translate.readMore') }}
                     </a>
+                    </div>
                 </div>
 
-                <div class="swiper-slide  max-w-[400px] h-[560px] p-6 rounded-xl shadow-lg bg-[#2b2a2a] flex flex-col fade-in"
+                <div class="swiper-slide  max-w-[400px] h-[660px] p-6 rounded-xl shadow-lg bg-[#2b2a2a] flex flex-col fade-in"
                     itemscope itemtype="http://schema.org/Service">
-                    <svg class="w-10 h-10 mb-4 text-white" fill="none" stroke="currentColor" stroke-width="2"
+                    <div class="flex flex-col h-full">
+                        <svg class="w-10 h-10 mb-4 text-white" fill="none" stroke="currentColor" stroke-width="2"
                         viewBox="0 0 24 24">
                         <path d="M18 13V8a4 4 0 10-8 0v5" />
                         <path d="M6 17h12" />
@@ -66,72 +78,80 @@
                         <circle cx="18" cy="17" r="2" />
                     </svg>
                     <h3 class="text-6xl font-bold mb-4" itemprop="name">{{ __('translate.servTitle4') }}</h3>
-                    <p class="text-3xl flex-grow mb-4" itemprop="description">
+                    <p class="text-3xl flex-grow mb-4 m-0" itemprop="description">
                         {{ __('translate.servDesc4') }}
                     </p>
-                    <a href="/{{ $lang }}/services-bitrix"
-                        class="inline-block !px-10 !py-6 bg-[#bc2c2c] hover:bg-[#e31e24] rounded font-bold text-white text-2xl">
+                    <a href="/{{ $lang }}/portfolio"
+                        class="mt-auto w-fit !px-10 !py-6 bg-[#bc2c2c] hover:bg-[#e31e24] rounded font-bold text-white text-2xl mt-auto">
                         {{ __('translate.readMore') }}
                     </a>
+                    </div>
                 </div>
 
 
 
-                <div class="swiper-slide  max-w-[400px] h-[560px] p-6 rounded-xl shadow-lg bg-[#350000] flex flex-col fade-in"
+                <div class="swiper-slide  max-w-[400px] h-[660px] p-6 rounded-xl shadow-lg bg-[#350000] flex flex-col fade-in"
                     itemscope itemtype="http://schema.org/Service">
-                    <svg class="w-10 h-10 mb-4 text-white" fill="none" stroke="currentColor" stroke-width="2"
+                    <div class="flex flex-col h-full">
+                        <svg class="w-10 h-10 mb-4 text-white" fill="none" stroke="currentColor" stroke-width="2"
                         viewBox="0 0 24 24">
                         <path d="M3 4a1 1 0 011-1h16a1 1 0 011 1v16a1 1 0 01-1 1H4a1 1 0 01-1-1V4z" />
                         <path d="M3 8h18" />
                     </svg>
                     <h3 class="text-6xl font-bold mb-4" itemprop="name">{{ __('translate.servTitle5') }}</h3>
-                    <p class="text-3xl flex-grow mb-4" itemprop="description">
+                    <p class="text-3xl flex-grow mb-4 m-0" itemprop="description">
                         {{ __('translate.servDesc5') }}
                     </p>
                     <a href="/{{ $lang }}/portfolio"
-                        class="inline-block !px-10 !py-6 bg-[#bc2c2c] hover:bg-[#e31e24] rounded font-bold text-white text-2xl">
+                        class="mt-auto w-fit !px-10 !py-6 bg-[#bc2c2c] hover:bg-[#e31e24] rounded font-bold text-white text-2xl mt-auto">
                         {{ __('translate.readMore') }}
                     </a>
+                    </div>
                 </div>
 
-                <div class="swiper-slide  max-w-[400px] h-[560px] p-6 rounded-xl shadow-lg bg-[#bc2c2c] flex flex-col fade-in"
+                <div class="swiper-slide  max-w-[400px] h-[660px] p-6 rounded-xl shadow-lg bg-[#bc2c2c] flex flex-col fade-in"
                     itemscope itemtype="http://schema.org/Service">
-                    <svg class="w-10 h-10 mb-4 text-white" fill="none" stroke="currentColor" stroke-width="2"
+                    <div class="flex flex-col h-full">
+                        <svg class="w-10 h-10 mb-4 text-white" fill="none" stroke="currentColor" stroke-width="2"
                         viewBox="0 0 24 24">
                         <rect x="7" y="2" width="10" height="20" rx="2" />
                         <path d="M11 18h2" />
                     </svg>
                     <h3 class="text-6xl font-bold mb-4" itemprop="name">{{ __('translate.servTitle6') }}</h3>
-                    <p class="text-3xl flex-grow mb-4" itemprop="description">
+                    <p class="text-3xl flex-grow mb-4 m-0" itemprop="description">
                         {{ __('translate.servDesc6') }}
                     </p>
-                    <a href="/{{ $lang }}/services-mobileapps"
-                        class="inline-block !px-10 !py-6 bg-[#350000] hover:bg-[#e31e24] rounded font-bold text-white text-2xl">
+                    <a href="/{{ $lang }}/portfolio"
+                        class="mt-auto w-fit !px-10 !py-6 bg-[#350000] hover:bg-[#e31e24] rounded font-bold text-white text-2xl mt-auto">
                         {{ __('translate.readMore') }}
                     </a>
+                    </div>
                 </div>
 
-                <div class="swiper-slide  max-w-[400px] h-[560px] p-6 rounded-xl shadow-lg bg-[#e31e24] flex flex-col fade-in"
+                <div class="swiper-slide  max-w-[400px] h-[660px] p-6 rounded-xl shadow-lg bg-[#e31e24] flex flex-col fade-in"
                     itemscope itemtype="http://schema.org/Service">
-                    <svg class="w-10 h-10 mb-4 text-white" fill="none" stroke="currentColor" stroke-width="2"
+                    <div class="flex flex-col h-full">
+                        <svg class="w-10 h-10 mb-4 text-white" fill="none" stroke="currentColor" stroke-width="2"
                         viewBox="0 0 24 24">
                         <circle cx="9" cy="21" r="1" />
                         <circle cx="20" cy="21" r="1" />
                         <path d="M1 1h4l2.68 13.39a2 2 0 002 1.61h9.72a2 2 0 002-1.61L23 6H6" />
                     </svg>
                     <h3 class="text-6xl font-bold mb-4" itemprop="name">{{ __('translate.servTitle7') }}</h3>
-                    <p class="text-3xl flex-grow mb-4" itemprop="description">
+                    <p class="text-3xl flex-grow mb-4 m-0" itemprop="description">
                         {{ __('translate.servDesc7') }}
                     </p>
-                    <a href="/{{ $lang }}/services-ecommerce"
-                        class="inline-block !px-10 !py-6 bg-[#350000] hover:bg-[#bc2c2c] rounded font-bold text-white text-2xl">
+                    <a href="/{{ $lang }}/portfolio"
+                        class="mt-auto w-fit !px-10 !py-6 bg-[#350000] hover:bg-[#bc2c2c] rounded font-bold text-white text-2xl mt-auto">
                         {{ __('translate.readMore') }}
                     </a>
+                    </div>
                 </div>
 
-                <div class="swiper-slide  max-w-[400px] h-[560px] p-6 rounded-xl shadow-lg bg-[#2b2a2a] flex flex-col fade-in"
+                <div class="swiper-slide  max-w-[400px] h-[660px] p-6 rounded-xl shadow-lg bg-[#2b2a2a] flex flex-col fade-in"
                     itemscope itemtype="http://schema.org/Service">
-                    <svg class="w-10 h-10 mb-4 text-white" fill="none" stroke="currentColor" stroke-width="2"
+                    <div class="flex flex-col h-full">
+                        <svg class="w-10 h-10 mb-4 text-white" fill="none" stroke="currentColor" stroke-width="2"
                         viewBox="0 0 24 24">
                         <path d="M18 13V8a4 4 0 10-8 0v5" />
                         <path d="M6 17h12" />
@@ -139,33 +159,40 @@
                         <circle cx="18" cy="17" r="2" />
                     </svg>
                     <h3 class="text-6xl font-bold mb-4" itemprop="name">{{ __('translate.servTitle8') }}</h3>
-                    <p class="text-3xl flex-grow mb-4" itemprop="description">
+                    <p class="text-3xl flex-grow mb-4 m-0" itemprop="description">
                         {{ __('translate.servDesc8') }}
                     </p>
-                    <a href="/{{ $lang }}/services-bitrix"
-                        class="inline-block !px-10 !py-6 bg-[#bc2c2c] hover:bg-[#e31e24] rounded font-bold text-white text-2xl">
+                    <a href="/{{ $lang }}/portfolio"
+                        class="mt-auto w-fit !px-10 !py-6 bg-[#bc2c2c] hover:bg-[#e31e24] rounded font-bold text-white text-2xl mt-auto">
                         {{ __('translate.readMore') }}
                     </a>
+                    </div>
                 </div>
 
-                <div class="swiper-slide  max-w-[400px] h-[560px] p-6 rounded-xl shadow-lg bg-[#bc2c2c] flex flex-col fade-in"
+                <div class="swiper-slide  max-w-[400px] h-[660px] p-6 rounded-xl shadow-lg bg-[#bc2c2c] flex flex-col fade-in"
                     itemscope itemtype="http://schema.org/Service">
-                    <svg class="w-10 h-10 mb-4 text-white" fill="none" stroke="currentColor" stroke-width="2"
+                    <div class="flex flex-col h-full">
+                        <svg class="w-10 h-10 mb-4 text-white" fill="none" stroke="currentColor" stroke-width="2"
                         viewBox="0 0 24 24">
                         <rect x="7" y="2" width="10" height="20" rx="2" />
                         <path d="M11 18h2" />
                     </svg>
                     <h3 class="text-6xl font-bold mb-4" itemprop="name">{{ __('translate.servTitle9') }}</h3>
-                    <p class="text-3xl flex-grow mb-4" itemprop="description">
+                    <p class="text-3xl flex-grow mb-4 m-0" itemprop="description">
                         {{ __('translate.servDesc9') }}
                     </p>
-                    <a href="/{{ $lang }}/services-mobileapps"
-                        class="inline-block !px-10 !py-6 bg-[#350000] hover:bg-[#e31e24] rounded font-bold text-white text-2xl">
+                    <a href="/{{ $lang }}/portfolio"
+                        class="mt-auto w-fit !px-10 !py-6 bg-[#350000] hover:bg-[#e31e24] rounded font-bold text-white text-2xl mt-auto">
                         {{ __('translate.readMore') }}
                     </a>
+                    </div>
                 </div>
 
 
+
+
+
+                
                 
             </div>
         </div>
